fdbdc198c762878453e2b2f79cb6a85a495da215
Austin Rivas <austinrivas@gmail.com>
2020-06-06 19:54:00 +0700

    renamed handler tests

--- template/deno-http/function/handler.test.ts
+++ template/deno-http/function/handler.test.ts
@@ -2,11 +2,11 @@ import { assert, assertEquals } from "https://deno.land/std/testing/asserts.ts";
 import { ServerRequest } from "https://deno.land/std/http/server.ts";
 import handler from "./handler.ts";
 
-Deno.test("handler: exists", () => {
+Deno.test("handler-template: exists", () => {
   assert(handler);
 });
 
-Deno.test("handler: returns Hello World", async () => {
+Deno.test("handler-template: returns Hello World", async () => {
   const req = new ServerRequest();
   const response = await handler(req);
   assertEquals(response.body, "Hello World");
